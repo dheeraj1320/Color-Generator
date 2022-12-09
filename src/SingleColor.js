a60801ea5b43f725feebeac2add6be52e0ac1e47
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2022-12-10 01:57:02 +0530

    first commit

---
+++ src/SingleColor.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index }) => {
+  return <h4>single color</h4>;
+};
+
+export default SingleColor;
